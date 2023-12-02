c26cfe73c33e202a614ad4011f681a51e8709d8b
Rajeshwari Shetty R <138014034+RajeshwariShettyR@users.noreply.github.com>
2023-12-02 11:09:34 +0000

    modified variables

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -3,5 +3,6 @@ let accEmail = "rs@gmail.com";
 var accPassword = 123456;
 accCity = "bengaluru";
 //avoid using var due to its issue in block scope and functional scope
+//const variable cannot be changed.
 console.log(accId);
 console.table([accId ,accEmail,accPassword ,accCity]);
