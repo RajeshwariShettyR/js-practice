e8bc1b8f253ef9965f766296fb8b46acbaedd939
Rajeshwari Shetty R <138014034+RajeshwariShettyR@users.noreply.github.com>
2023-12-02 11:04:50 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,7 @@
+const accId =141;
+let accEmail = "rs@gmail.com";
+var accPassword = 123456;
+accCity = "bengaluru";
+//avoid using var due to its issue in block scope and functional scope
+console.log(accId);
+console.table([accId ,accEmail,accPassword ,accCity]);
